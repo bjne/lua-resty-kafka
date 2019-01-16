589e12c192c1ba72427e5dcb0c7fe64ce6f4062a
Bjørnar Ness <bjornar.ness@gmail.com>
2019-01-16 19:10:48 +0100

    cleanup and simplify

--- lib/kafka/produce.lua
+++ lib/kafka/produce.lua
@@ -31,7 +31,6 @@ local stat, worker_count = ngx.shared.kafka_stats, ngx.worker.count()
 local stat_incr, stat_rate = function()end, function()end
 
 
-
 local function get_free_record_batch(self)
 	local cur_record_batch = self.record_batch
 
@@ -120,13 +119,12 @@ function _M.new(config)
 		return nil, err
 	end
 
-
 	local self = {
 		client                 = client,
 
 		topic                  = config.topic,
 		client_id              = config.client_id,
-		partition							 = config.partition,
+		partition              = config.partition,
 
 		batch_num              = config.batch_num or 4,
 		batch_size             = config.batch_size or 1024 * 1024,
@@ -136,7 +134,7 @@ function _M.new(config)
 		timeout                = config.timeout or 1500,
 		max_timeout            = config.max_timeout or 2000,
 		zstd_compression_level = config.zstd_compression_level or 9,
-		flush_interval				 = config.flush_interval or 2,
+		flush_interval         = config.flush_interval or 2,
 
 		offset = 8,
 		error_code_offset      = 4 + 4 + 2 + #config.topic + 4 + 4
@@ -145,19 +143,6 @@ function _M.new(config)
 	if type(config.partition or 0) == "number" then
 		self.partition = function() return config.partition or 0 end
 	end
-	ngx.log(ngx.ERR, "partition: ", self.partition())
-
-	local request_header, produce_header = {
-		char(0x00, 0x00),                    -- api_key
-		char(0x00, 0x07),                    -- api_version
-		char(0,0,0,0),                       -- correlation_id
-		encode.string(self.client_id)        -- client_id
-	}, {
-		encode.string(""),                   -- transaction_id
-		encode.int16(self.required_acks),    -- required_acks
-		encode.int32(self.timeout),          -- timeout
-		encode.int32(1),                     -- number of topics
-	}
 
 	-- (request_header) + (produce_header)
 	self.header_length = (10 + #self.client_id) + (26 + #self.topic)
@@ -175,66 +160,69 @@ function _M.new(config)
 			record_batch.next = i == self.batch_num and record_batch_1
 		end
 
-		record_batch[1] = encode.int16(4)    -- attributes (zstd)
-		record_batch[5] = encode.int64(-1)   -- producer_id
-		record_batch[6] = encode.int16(-1)   -- producer_epoch
-		record_batch[7] = encode.int32(-1)   -- first_sequence (not used)
+		record_batch[1] = encode.int16(4)              -- attributes (zstd)
+		record_batch[5] = encode.int64(-1)             -- producer_id
+		record_batch[6] = encode.int16(-1)             -- producer_epoch
+		record_batch[7] = encode.int32(-1)             -- first_sequence
 
 		record_batch.size, record_batch.length, record_batch.pkt = 0, 0, {
-			[01] = nil,                       -- size (int32),
-			[02] = concat(request_header),
-			[03] = concat(produce_header),
-			[04] = encode.string(self.topic), -- config.topic
-			[05] = encode.int32(1),           -- num_partitions
-			[06] = nil,                       -- partition_id (int32)
-			[07] = nil,                       -- message_set_size (int32)
+			[01] = nil,                                -- size (int32),
+			[02] = concat{
+			         char(0x00, 0x00),                 -- api_key
+			         char(0x00, 0x07),                 -- api_version
+			         char(0,0,0,0),                    -- correlation_id
+			         encode.string(self.client_id)     -- client_id
+			       },
+			[03] = concat{
+			         encode.string(""),                -- transaction_id
+			         encode.int16(self.required_acks), -- required_acks
+			         encode.int32(self.timeout),       -- timeout
+			         encode.int32(1)                   -- number of topics
+			       },
+			[04] = encode.string(self.topic),          -- config.topic
+			[05] = encode.int32(1),                    -- num_partitions
+			[06] = nil,                                -- partition_id (int32)
+			[07] = nil,                                -- num_records (int32)
 
-			[08] = encode.int64(0),           -- offset
-			[09] = nil,                       -- size in bytes (int32)
-			[10] = encode.int32(-1),          -- partition leader epoch
-			[11] = char(0x02),                -- magic
-			[12] = nil,                       -- crc32c (int32)
+			[08] = encode.int64(0),                    -- offset
+			[09] = nil,                                -- size in bytes (int32)
+			[10] = encode.int32(-1),                   -- partition leader epoch
+			[11] = char(0x02),                         -- magic
+			[12] = nil,                                -- crc32c (int32)
 
-			[13] = nil                        -- data
+			[13] = nil                                 -- data
 		}
 	end
 
-
-	local hdl, err = ngx.timer.every(self.flush_interval,
-		function(premature, self)
-			local first_timestamp = self.record_batch.first_timestamp or math.huge
-			return first_timestamp + self.flush_interval <= ngx.now() and self:send()
-		end,
-	self)
+	ngx.timer.every(self.flush_interval, function(_, self, record_batch)
+		if record_batch.first_timestamp + self.flush_interval < ngx.now() then
+			return self:send()
+		end
+	end, self, record_batch)
 
 	return setmetatable(self, mt)
 end
 
 function _M:send()
-	local record_batch, idx = self.record_batch
+	local record_batch, idx, pkt, data, size = self.record_batch
 
 	if record_batch.lock or record_batch.length == 0 then
 		return
 	end
 
-	record_batch.lock, idx = true, record_batch.length + self.offset
-	record_batch[idx + 1], record_batch[idx + 2] = zstd:finalize()
+	record_batch.lock, idx = true, record_batch.length + self.offset + 1
+	record_batch[idx] = zstd:finalize()
 
 	record_batch[2] = encode.int32(record_batch.length - 1)
 	record_batch[3] = encode.int64(record_batch.first_timestamp)
 	record_batch[4] = encode.int64(record_batch.max_timestamp)
 	record_batch[8] = encode.int32(record_batch.length)
 
-	local pkt, data = record_batch.pkt, concat(record_batch, "", 1, record_batch.length + self.offset + 1)
-
-	--local pkt, data = record_batch.pkt, concat(record_batch)
-	local size = #data + 9                    -- record batch header
+	data, pkt = concat(record_batch, "", 1, idx), record_batch.pkt
+	size = #data + 9
 
 	stat_rate('compression ratio', record_batch.size / (size - 49))
 	stat_rate('average records', record_batch.length)
-
-
-	--local partition = self.partition(record_batch)
 
 	pkt[01] = encode.int32(size + 12 + self.header_length)
 	pkt[06] = self.partition(record_batch)    -- partition_id
@@ -248,7 +236,6 @@ function _M:send()
 		record_batch.pkt[06] = encode.int32(partition)
 
 		for i=1,2 do
-
 			data, err = self.client:send(record_batch.pkt, partition, i==2)
 
 			if not data then
@@ -292,7 +279,6 @@ function _M:add(data, size)
 	-- size + key_length + attributes
 	record[1], size = encode.varint(size + 2, size + 2)
 
-
 	if size + record_batch.size >= self.batch_size then
 		record_batch = self:send()
 
@@ -312,8 +298,7 @@ function _M:add(data, size)
 	record_batch.length = record_batch.length + 1
 	record_batch.size = record_batch.size + size
 
-	local idx = record_batch.length + self.offset
-	record_batch[idx], record_batch[idx + 1] = zstd:update(concat(record))
+	record_batch[record_batch.length + self.offset] = zstd:update(record)
 
 	return record_batch.length >= self.batch_length and self:send()
 end
